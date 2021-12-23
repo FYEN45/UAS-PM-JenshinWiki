f41ffcf9009e941ce8dce12753c8a55be22d317c
FYEN45 <ferry.ahogun@gmail.com>
2021-12-23 09:13:48 +0700

    Nambahin File doang.
    Blm ngoding sama skali.

---
+++ MYSQL_PHP/JenshinWiki-ws/connect.php
@@ -0,0 +1,31 @@
+<?php
+    error_reporting(0);
+    require __DIR__. '/config.php';
+    
+    class DB_CONNECT {
+        public $con;
+
+        function __construct()
+        {
+            $this->connect_DB();
+        }
+
+        function __destruct()
+        {
+            $this->close_DB();
+        }
+
+        function connect_DB(){
+            $this->con = mysqli_connect(DB_SERVER, DB_USER, DB_PASSWORD, DB_DATABASE);
+            if(mysqli_connect_errno()){
+                printf("Koneksi Gagal! \n", mysqli_connect_error());
+                exit();
+            }
+            return $this->con;
+        }
+
+        function close_DB(){
+            mysqli_close($this->con);
+        }
+    }
+?>
---
+++ app/src/main/java/com/example/jenshinwiki/Config/Config.java
@@ -0,0 +1,11 @@
+package com.example.jenshinwiki.Config;
+
+public interface Config {
+
+    public String serverIp = "192.168.1.13";    //ServerIP untuk mengakses server local
+
+    public String userLoginVerification = "";
+    public String userRegister          = "";
+    public String userListRequest       = "";
+    public String userUpdateRequest     = "";
+}
