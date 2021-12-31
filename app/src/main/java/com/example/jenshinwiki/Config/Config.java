1c69e8bf6f4278e489ac0d53622eeecb2c218792
FYEN45 <ferry.ahogun@gmail.com>
2021-12-31 13:10:24 +0700

    Update Config.java

--- app/src/main/java/com/example/jenshinwiki/Config/Config.java
+++ app/src/main/java/com/example/jenshinwiki/Config/Config.java
@@ -2,10 +2,10 @@ package com.example.jenshinwiki.Config;
 
 public interface Config {
 
-    //Penjelasan : serverIp digunakan untuk menentukan alamat IP untuk mengakses webservices.
+    //Variabel serverIp digunakan untuk menentukan alamat IP untuk mengakses webservices.
     String serverIp = "192.168.159.202";
 
-    //Penjelasan : Baris - baris kode berikut digunakan untuk menghubungkan aplikasi dengan webservices
+    //Baris - baris kode berikut digunakan untuk menghubungkan aplikasi dengan webservices
     // sehingga aplikasi dapat mengirim maupun menerima respon dari server yang dapat mengakses database MySQL.
     String userLoginVerification = "http://" + serverIp + "/JenshinWiki-ws/userLoginVerification.php/";
     String userRegister = "http://" + serverIp + "/JenshinWiki-ws/userRegister.php/";
